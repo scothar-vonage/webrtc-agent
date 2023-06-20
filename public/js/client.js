cc2fb32a078aa985f74b1739ba4e56d60a04b0ea
Scott Harris <sharris@twilio.com>
2023-06-20 15:07:29 -0700

    Added inbound calling

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   // Fetch a JWT from the server to authenticate the user
-  const response = await fetch('/auth/supportuser');
+  const response = await fetch("/auth/supportuser");
   const jwt = await response.json();
+  let blink = false;
 
   // Create a new NexmoClient instance and authenticate with the JWT
   let client = new NexmoClient();
@@ -10,38 +11,69 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   let conversation = null;
 
-  // Whenever a call is made bind an event that ends the call to
-  // the hangup button
+  /*
+   * Process call events
+   */
   application.on("member:call", (member, call) => {
+    console.log("receiving call.");
+
     let terminateCall = () => {
       call.hangUp();
-      toggleCallStatusButton('idle');
-      btnHangup.removeEventListener('click', terminateCall)
+      toggleCallStatusButton("idle");
+      btnHangup.removeEventListener("click", terminateCall);
     };
-    btnHangup.addEventListener('click', terminateCall);
 
-    // Retrieve the Conversation so that we can determine if a 
-    // Member has left and refresh the button state
+    let answerCall = () => {
+      call.answer();
+      btnAnswer.classList.remove("button-blink");
+      toggleCallStatusButton("in_progress");
+    };
+
+    /*
+     * If the call is inbound, flash the "Answer" button until it is answered
+     */
+    if (call.direction === "inbound") {
+      toggleCallStatusButton("ringing");
+    }
+
+    btnAnswer.addEventListener("click", answerCall);
+    btnHangup.addEventListener("click", terminateCall);
+
+    /*
+     * Retrieve the Conversation so that we can determine if a
+     * Member has left and refresh the button state
+     */
     conversation = call.conversation;
     conversation.on("member:left", (member, event) => {
-      toggleCallStatusButton('idle');
+      toggleCallStatusButton("idle");
     });
   });
 
-  // Whenever we click the call button, trigger a call to the support number
-  // and hide the Call Now button
-  btnCall.addEventListener('click', () => {
+  /*
+   *Whenever we click the call button, trigger a call to the support number
+   * and hide the Call Now button
+   */
+  btnCall.addEventListener("click", () => {
     application.callServer(application.me.name);
-    toggleCallStatusButton('in_progress');
+    toggleCallStatusButton("in_progress");
   });
 });
 
 function toggleCallStatusButton(state) {
-  if (state === 'in_progress') {
-    btnCall.style.display = "none";
-    btnHangup.style.display = "inline-block";
-  } else {
-    btnCall.style.display = "inline-block";
-    btnHangup.style.display = "none";
+  switch (state) {
+    case "idle":
+      btnCall.style.display = "inline-block";
+      btnAnswer.style.display = "inline-block";
+      btnHangup.style.display = "none";
+      btnAnswer.classList.remove("button-blink");
+      break;
+    case "in_progress":
+      btnCall.style.display = "none";
+      btnHangup.style.display = "inline-block";
+      btnAnswer.classList.remove("button-blink");
+      break;
+    case "ringing":
+      btnAnswer.classList.add("button-blink");
+      break;
   }
 }
--- server.js
+++ server.js
@@ -1,51 +1,84 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const nexmo = require('nexmo-client');
+const bodyParser = require("body-parser");
+const nexmo = require("nexmo-client");
+const { generateToken } = require("./utils");
 
-require('dotenv').config();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use(express.static('public'))
-app.use('/modules', express.static('node_modules/nexmo-client/dist/'));
+app.use(express.static("public"));
+app.use("/modules", express.static("node_modules/nexmo-client/dist/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/auth/:userid', (req, res) => {
-  console.log(`Authenticating ${req.params.userid}`)
-  return res.json(process.env.JWT);
-})
+app.get("/auth/:userid", (req, res) => {
+  console.log(`Authenticating ${req.params.userid}`);
+  const jwt = generateToken(req.params.userid);
+  return res.json(jwt);
+});
 
-app.get('/webhooks/answer', (req, res) => {
-  console.log("Answer:")
-  console.log(req.query)
-  const ncco = [
-    {
-      "action": "talk",
-      "text": "Thank you for calling Acme support. Transferring you now."
-    },
-    {
-      "action": "connect",
-      "from": process.env.NEXMO_NUMBER,
-      "endpoint": [{
-        "type": "phone",
-        "number": process.env.DESTINATION_PHONE_NUMBER
-      }]
-    }]
+app.get("/webhooks/answer", (req, res) => {
+  console.log("Answer:");
+  console.log(req.query);
+  let ncco;
+
+  const to = req.query.to;
+
+  /*
+   * This is an inbound call which I will connect
+   * to my app user via WebRTC
+   */
+  if (to === "18335808845") {
+    ncco = [
+      {
+        action: "talk",
+        text: "Please wait while we connect you .",
+      },
+      {
+        action: "connect",
+        from: req.query.from,
+        endpoint: [
+          {
+            type: "app",
+            user: "supportuser",
+          },
+        ],
+      },
+    ];
+  } else {
+    /*
+     * This is an outbound call from the app user to a
+     * destination phone number.
+     */
+    ncco = [
+      {
+        action: "talk",
+        text: "Connecting you to the customer now.",
+      },
+      {
+        action: "connect",
+        from: process.env.VONAGE_NUMBER,
+        endpoint: [
+          {
+            type: "phone",
+            number: process.env.DESTINATION_PHONE_NUMBER,
+          },
+        ],
+      },
+    ];
+  }
   res.json(ncco);
 });
 
-app.post('/webhooks/event', (req, res) => {
-  console.log("EVENT:")
-  console.log(req.body)
-  res.status(200).end()
+app.post("/webhooks/event", (req, res) => {
+  //console.log("EVENT:");
+  //console.log(req.body);
+  res.status(200).end();
 });
-
-
-
